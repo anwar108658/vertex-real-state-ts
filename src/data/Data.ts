0dda68345df41d70fa89168669d0909d708ae243
anwar108658 <anwar108658@gmail.com>
2025-11-08 17:23:49 +0500

    add some data

--- src/data/Data.ts
+++ src/data/Data.ts
@@ -42,5 +42,51 @@ export const navBarData = [
         link: "/contact",
         icon: null,
         children: null,
+    },
+    {
+        id: 7,
+        title: "Market Intelligence",
+        link: "/market-intelligence",
+        icon: null,
+        children: [
+            {
+                id: 1,
+                title: "Property Prices",
+                link: "/market-intelligence/property-prices",
+                icon: null,
+            },
+            {
+                id: 2,
+                title: "Trends",
+                link: "/market-intelligence/trends",
+                icon: null,
+            }
+        ],
+    },
+    {
+        id: 8,
+        title: "Guides",
+        link: "/guides",
+        icon: null,
+        children: [
+            {
+                id: 1,
+                title: "Buying Guide",
+                link: "/guides/buying-guide",
+                icon: null,
+            },
+            {
+                id: 2,
+                title: "Selling Guide",
+                link: "/guides/selling-guide",
+                icon: null,
+            },
+            {
+                id: 3,
+                title: "Renting Guide",
+                link: "/guides/renting-guide",
+                icon: null,
+            }
+        ],
     }
 ];
